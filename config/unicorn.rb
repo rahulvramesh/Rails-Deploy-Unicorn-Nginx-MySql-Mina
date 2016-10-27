bf36e86d3558ad10ddd29662bc5273b262d287c0
rahul <rahulvramesh@hotmail.com>
2016-10-27 17:41:29 +0000

    initial push

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/ubuntu/example"
+working_directory root
+pid "#{root}/shared/pids/unicorn.pid""
+stderr_path "#{root}/shared/log/unicorn.stderr.log"
+stdout_path "#{root}/shared/log/unicorn.stdout.log"
+
+listen "/home/ubuntu/example/shared/sockets/unicorn.sock"
+worker_processes 2
+timeout 30
